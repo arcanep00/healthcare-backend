5442dbbc32e836a5bb910f63262e9740a8f01e2b
Abhinav Pandey <navabhiday00@gmail.com>
2026-06-18 20:28:13 +0530

    chore: add environment variable configuration

--- healthcare_backend/settings.py
+++ healthcare_backend/settings.py
@@ -11,19 +11,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mqin2=4m$r0ssa^^k=%2p3*o&ea&hwu73hu!(_5crn@=qq(^a7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -80,11 +82,16 @@ WSGI_APPLICATION = 'healthcare_backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "healthcare_db",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
+
+        "NAME": os.getenv("DB_NAME"),
+
+        "USER": os.getenv("DB_USER"),
+
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+
+        "HOST": os.getenv("DB_HOST"),
+
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 REST_FRAMEWORK = {
